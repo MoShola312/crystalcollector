25ea9c61ccccf21448987826e6fb66022d903a2b
Mohammed S <37146251+MoShola312@users.noreply.github.com>
2019-02-12 11:43:51 -0600

    Add files via upload

--- ccollector.js
+++ ccollector.js
@@ -1,17 +1,84 @@
 var goal = Math.floor(Math.random() * 100);
 var currentScore = 0;
-
+var win = 0;
+var lose = 0;
+var plusPurple = Math.floor(Math.random() * 40);
+console.log("purle " + plusPurple);
+var plusGreen = Math.floor(Math.random() * 50);
+console.log("green " + plusGreen);
+var plusEmerald = Math.floor(Math.random() * 30);
+console.log("emerald " + plusEmerald);
+var plusRed = Math.floor(Math.random() * 20);
+console.log("red " + plusRed);
 
 $("#goal").text(goal);
-$("#currentScore").text(currentScore)
 
 $("#red").on("click", function() {
   addRed()
+  $("#currentScore").html(currentScore)
 })
 
-var plusRed = Math.floor(Math.random() * 100);
+$("#green").on("click", function() {
+  addGreen()
+  $("#currentScore").html(currentScore)
+})
+
+$("#purple").on("click", function() {
+  addPurple()
+  $("#currentScore").html(currentScore)
+})
+
+$("#emerald").on("click", function() {
+  addEmerald()
+  $("#currentScore").html(currentScore)
+})
+
+
+
+
 function addRed(){
-  console.log(plusRed);
   currentScore += plusRed;
-  console.log("red " + currentScore)
+  console.log("Score " + currentScore);
+  return currentScore;
+}
+
+function addGreen(){
+  currentScore += plusGreen;
+  console.log("Score " + currentScore);
+  return currentScore;
+}
+
+function addEmerald(){
+  currentScore += plusEmerald;
+  console.log("Score " + currentScore);
+  return currentScore;
+}
+
+function addPurple(){
+  currentScore += plusPurple;
+  console.log("Score " + currentScore);
+  return currentScore;
+}
+
+if(currentScore === goal){
+  win++;
+  $("#win").html(win);
+  $("#total").append("<p>You Win!</p>");
+  endGame();
+} else if (currentScore > goal){
+  lose++;
+  $("#lose").html(lose);
+  $("#total").append("<p>You Lose</p>");
+  endGame();
+}
+console.log("goal: " + goal);
+console.log("C: " + currentScore);
+
+function endGame() {
+  plusPurple = Math.floor(Math.random() * 40);
+  plusGreen = Math.floor(Math.random() * 50);
+  plusEmerald = Math.floor(Math.random() * 30);
+  plusRed = Math.floor(Math.random() * 20);
+  goal = Math.floor(Math.random() * 100);
+  currentScore = 0;
 }
